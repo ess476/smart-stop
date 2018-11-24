00bf8f2c67e302e1c61aaec6ef445f1714d70661
Ethan Sattler <mcdasethan2@gmail.com>
2018-11-24 02:59:04 -0500

    Got stop and resume buttons working

---
+++ index.js
@@ -0,0 +1,124 @@
+
+var start = 0
+var end = 0
+
+var depth = 0;
+var cond_depth = 0;
+var stopped = false;
+
+function resumePressed()
+{
+	return new Promise((resolve, reject) => {
+		$("#resume").click(function(){
+			stopped = false;
+			resolve();
+		});
+	});
+}
+
+function depthdec()
+{
+	depth -= 1;
+}
+
+async function check()
+{
+	if (stopped)
+	{
+		await resumePressed();
+
+		return new Promise((resolve, reject) => {
+			setTimeout(resolve, 0);
+		});
+
+	} else {
+
+		if (depth <= 8000)
+		{
+			depth += 1;
+			return;
+		}
+
+		depth = 0;
+		return new Promise((resolve, reject) => {
+			setTimeout(resolve, 0);
+		});
+	}
+}
+
+
+
+async function fapply(f, ...args) {
+	await check();
+	return f(...args);
+}
+
+async function sum(x, acc) {
+	await check();
+	if (x === 0) {
+		return acc;
+	} else {
+		return fapply(sum, x-1, acc + x);
+	}
+}
+
+function run()
+{
+
+	checkcall = esprima.parse('async function func() { await check(); } ').body[0].body.body[0];
+	console.log(checkcall);
+	parsed = esprima.parse($('#code').val(), {tolerant: true}, function (node, meta) {
+		if (node.type == "FunctionDeclaration")
+		{
+			node.async = true;
+			node.body.body.unshift(checkcall);
+		} else if (node.type == "CallExpression")
+		{
+			call = jQuery.extend(true, {}, node);
+
+			node.type = "AwaitExpression";
+			node.argument = call;
+
+			delete node["arguments"];
+			delete node["callee"];
+
+			console.log(node);
+		} else {
+			//console.log(node);
+		}
+	});
+
+	
+	
+
+	let code = escodegen.generate(parsed);
+
+	code = "async function _stub() { " +  code + "} setTimeout(_stub, 0);";
+	code = code.replace('await,', 'await');
+	console.log(code);
+	eval(code);
+}
+
+
+$(document).ready(function()
+{
+
+	$('#stop').click(function() {
+		stopped = true;
+		alert('stopped');
+	})
+
+	$('#run').click(async function()
+	{
+		start = performance.now();
+
+		run();
+
+		end = performance.now();
+		
+		console.log('time: ' + (end - start));
+
+		//inf();
+	});
+});
+
